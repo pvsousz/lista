7521b24a124074c03eff1603ac3cb378222a9ece
Paulo Vinicius Paulino de Sousa <110861619+pvsousz@users.noreply.github.com>
2023-07-02 00:00:57 -0300

    Add files via upload

---
+++ 2506.c
@@ -0,0 +1,38 @@
+
+
+#include <stdio.h>
+
+#define true 1
+#define false 0
+
+int main ()
+{
+
+    short i, ans;
+    short hora, minuto, critico, n;
+
+    while (scanf("%hd", &n) != EOF)
+    {   
+
+        ans = 0;
+        short tempo_total = 420;
+        for (i = 0; i < n; ++i)
+        {
+
+            scanf("%hd %hd %hd", &hora, &minuto, &critico);
+
+            while (tempo_total < (hora * 60) + minuto)
+                tempo_total += 30;
+            
+            if (tempo_total - ((hora * 60) + minuto) > critico)
+                ++ans;
+
+            tempo_total += 30;
+
+        }
+
+         printf("%hd\n", ans);
+        
+    }
+
+}
